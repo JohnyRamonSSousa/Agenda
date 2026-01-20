94819410caee029b05fdc44b46d6e01d73c23206
JohnyRamonSSousa <erikaandradesantosousa@gmail.com>
2026-01-20 19:59:56 -0300

    iniciando projeto de agenda

---
+++ create.php
@@ -0,0 +1,13 @@
+<?php
+
+ include_once("templates/header.php");
+
+?>
+    <h1>Teste cração</h1>
+    
+    
+<?php
+
+ include_once("templates/footer.php");
+
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+ include_once("templates/header.php");
+
+?>
+    <h1>Testando agenda</h1>
+    
+    
+<?php
+
+ include_once("templates/footer.php");
+
+?>
